7c4dbf96eca2ccb85ccccc8474b8c4fce91ada32
Brian Takita <brian.takita@gmail.com>
2023-10-15 22:18:37 -0400

    - SITE_TITLE
    - SITE_DESCRIPTION
    
    SOCIALS: linkTitle=>link_title

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 // Place any global data in this file.
 import type { LogoImage, Site, Social } from '@btakita/domain--server--blog'
 // You can import this data from anywhere in your site by using the `import` keyword.
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
 export const SITE:Site = {
 	website: 'https://briantakita.me/', // replace this with your deployed domain
 	author: 'Brian Takita',
@@ -22,43 +20,43 @@ export const SOCIALS:Social[] = [
 	{
 		name: 'Github',
 		href: 'https://github.com/btakita',
-		linkTitle: ` ${SITE.title} on Github`,
+		link_title: ` ${SITE.title} on Github`,
 		active: true,
 	},
 	{
 		name: 'LinkedIn',
 		href: 'https://www.linkedin.com/in/briantakita/',
-		linkTitle: `${SITE.title} on LinkedIn`,
+		link_title: `${SITE.title} on LinkedIn`,
 		active: true,
 	},
 	{
 		name: 'Mail',
 		href: 'mailto:brian.takita@gmail.com',
-		linkTitle: `Send an email to ${SITE.title}`,
+		link_title: `Send an email to ${SITE.title}`,
 		active: false,
 	},
 	{
 		name: 'Discord',
 		href: 'https://discord.com/users/413926819733962762',
-		linkTitle: `${SITE.title} on Discord`,
+		link_title: `${SITE.title} on Discord`,
 		active: false,
 	},
 	{
 		name: 'GitLab',
 		href: 'https://gitlab.com/btakita',
-		linkTitle: `${SITE.title} on GitLab`,
+		link_title: `${SITE.title} on GitLab`,
 		active: false,
 	},
 	{
 		name: 'Skype',
 		href: 'https://github.com/satnaing/astro-paper',
-		linkTitle: `${SITE.title} on Skype`,
+		link_title: `${SITE.title} on Skype`,
 		active: false,
 	},
 	{
 		name: 'Telegram',
 		href: 'https://t.me/BrianTakita',
-		linkTitle: `${SITE.title} on Telegram`,
+		link_title: `${SITE.title} on Telegram`,
 		active: false,
 	},
 ]
