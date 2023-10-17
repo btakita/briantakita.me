b73c29541d333cf2b43ae0011e44697370eb2088
Brian Takita <brian.takita@gmail.com>
2023-10-16 20:04:56 -0400

    server__ctx__new:
    	fix: url: ∋ SITE.website: - ∋ Astro.url.hostname:
    		∋ load__regular_font__set
    		∋ load__bold_font__set

--- src/lib/server__ctx.ts
+++ src/lib/server__ctx.ts
@@ -1,4 +1,9 @@
-import { blog_server__ctx__new, type load__font_T, load__regular_font__set } from '@btakita/domain--server--blog'
+import {
+	blog_server__ctx__new,
+	load__bold_font__set,
+	type load__font_T,
+	load__regular_font__set
+} from '@btakita/domain--server--blog'
 import { LOGO_IMAGE, SITE, SOCIALS } from '@config'
 import type { APIContext, AstroGlobal } from 'astro'
 export function server__ctx__new(Astro:APIContext|AstroGlobal) {
@@ -11,13 +16,13 @@ export function server__ctx__new(Astro:APIContext|AstroGlobal) {
 		name: 'Atkinson Hyperlegible',
 		weight: 400,
 		style: 'normal',
-		url: `https://${Astro.url.hostname}/assets/fonts/atkinson-regular.woff`,
+		url: new URL('/assets/fonts/atkinson-regular.woff', SITE.website).href,
 	} as load__font_T)
-	load__regular_font__set(ctx, {
+	load__bold_font__set(ctx, {
 		name: 'Atkinson Hyperlegible',
 		weight: 600,
 		style: 'normal',
-		url: `https://${Astro.url.hostname}/assets/fonts/atkinson-bold.woff`,
+		url: new URL('/assets/fonts/atkinson-bold.woff', SITE.website).href,
 	} as load__font_T)
 	return ctx
 }
