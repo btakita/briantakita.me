29df8acdcbf6df715602af880c937da140545dfc
Brian Takita <brian.takita@gmail.com>
2023-10-18 04:54:23 -0400

    + /posts/my-history-with-state-management-and-ctx-core-nanostores
    + /posts/personal-site-written-with-astro-solidjs-ctx-core-nanostores: draft
    
    /posts/backbone-signal-practical-reactive-programming-in-javascript: tags: + backbonejs
    
    /posts/everything-exists: ## instead of ###
    
    /portfolio: Rundavoo: + Google top sites
    
    /posts/[slug]: draft posts are viewable
    
    ∋ sorted__posts__new:
    
    	/
    	/posts
    	/rss.xml
    	/search
    	/tags/[tag]
    
    package.json: start: ./bin/dev.sh: + restart on crash

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,11 @@
 import rss from '@astrojs/rss'
+import type { Post } from '@btakita/domain--all--blog'
+import { sorted__posts__new } from '@btakita/domain--server--blog'
 import { SITE } from '@config'
 import type { APIContext } from 'astro'
 import { getCollection } from 'astro:content'
 export async function GET(Astro:APIContext) {
-	const posts = await getCollection('posts')
+	const posts = sorted__posts__new(await getCollection('posts') as Post[])
 	const items = posts.map(post=>({
 		...post.data,
 		link: `/blog/${post.slug}/`,
