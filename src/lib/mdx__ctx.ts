b9b88d5f272c90ed6ea25bc73068039cdf3625a3
Brian Takita <brian.takita@gmail.com>
2023-11-06 13:02:14 -0500

    dependencies:
    	∋ @ctx-core/vanjs
    	update dependencies

--- src/lib/mdx__ctx.ts
+++ src/lib/mdx__ctx.ts
@@ -1,10 +1,11 @@
-import { blog__ctx__new, van__set } from '@btakita/domain--all--blog'
+import { blog__ctx__new } from '@btakita/domain--all--blog'
 import { logo_image__set, site__set, socials__set } from '@btakita/domain--server--blog'
 import { LOGO_IMAGE, SITE, SOCIALS } from '@config'
+import { props_clean_van__new, van__set } from '@ctx-core/vanjs'
 import van from 'mini-van-plate/van-plate'
 export function mdx__ctx__new() {
 	const ctx = blog__ctx__new()
-	van__set(ctx, van)
+	van__set(ctx, props_clean_van__new(van))
 	logo_image__set(ctx, LOGO_IMAGE)
 	site__set(ctx, SITE)
 	socials__set(ctx, SOCIALS)
