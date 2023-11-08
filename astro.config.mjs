63d7e263034e997670bdda22e070151b920cdec8
Brian Takita <brian.takita@gmail.com>
2023-11-07 21:28:15 -0500

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://briantakita.me',
-	integrations: [mdx(), sitemap(), /*solidJs(),*/ tailwind()]
+	integrations: [mdx(), sitemap(), tailwind()]
 })
