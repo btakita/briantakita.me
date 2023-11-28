44e68b4c41f0622d6d6d95283dc9163bbd664884
Brian Takita <brian.takita@gmail.com>
2023-11-28 13:50:14 -0500

    + BRIANTAKITAME_PORT: 4100

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://briantakita.me',
-	integrations: [mdx(), sitemap(), tailwind()]
+	integrations: [mdx(), sitemap(), tailwind()],
+	server: {
+		host: '0.0.0.0', // heroku requires 0.0.0.0
+		port: parseInt(import.meta.env.BRIANTAKITAME_PORT)
+	},
 })
