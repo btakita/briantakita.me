3d27f88f6c9e581c696dda32fa0af7e5ccf798ef
Brian Takita <brian.takita@gmail.com>
2023-11-29 21:56:01 -0500

    ↑ relement
    	∋ relement/astro
    		- Rel.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://briantakita.me',
+	output: 'hybrid',
+	adapter: vercel(),
 	integrations: [mdx(), sitemap(), tailwind()],
 	...(import.meta.env.BRIANTAKITAME_PORT ? {
 		server: {
