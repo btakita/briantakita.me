e520051d68503077521b463d9951e85f140cd0f4
Brian Takita <brian.takita@gmail.com>
2024-01-31 02:08:22 -0500

    + ∋ @btakita/ui--browser--blog

--- src2/app/index.browser.ts
+++ src2/app/index.browser.ts
@@ -1,10 +1,10 @@
-import { blog_header__bind } from '@btakita/domain--browser--blog/header'
-import { theme_toggle_button__bind } from '@btakita/domain--browser--blog/theme'
+import { blog_header__bind } from '@btakita/ui--browser--blog/header'
+import { theme__toggle_button__bind } from '@btakita/ui--browser--blog/theme'
 import { relement__use } from 'relementjs'
 import { browser__relement, hy__bind } from 'relementjs/browser'
 relement__use(browser__relement)
 hy__bind(
 	document, {
 		blog_header: blog_header__bind,
-		theme_toggle_button: theme_toggle_button__bind,
+		theme_toggle_button: theme__toggle_button__bind,
 	})
