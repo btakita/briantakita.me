7e0317772a81254b4e2d0780a427731c8c996afe
Brian Takita <brian.takita@gmail.com>
2024-02-01 05:34:08 -0500

    + /posts/:slug_or_page_num: wip: + page_num

--- src2/app/index.server.ts
+++ src2/app/index.server.ts
@@ -1,5 +1,5 @@
 import './index.css'
-import { blog_server_request_ctx__ensure, tag__set } from '@btakita/domain--server--blog'
+import { blog_server_request_ctx__ensure, page_num__set, page_num_a1_, tag__set } from '@btakita/domain--server--blog'
 import { blog__rss_xml_ } from '@btakita/ui--server--blog/rss'
 import { about__doc_html_ } from '@btakita/ui--server--briantakita/about'
 import { home__doc_html_ } from '@btakita/ui--server--briantakita/home'
@@ -7,6 +7,7 @@ import { open_source__doc_html_ } from '@btakita/ui--server--briantakita/open_so
 import { portfolio__doc_html_ } from '@btakita/ui--server--briantakita/portfolio'
 import { posts__doc_html_ } from '@btakita/ui--server--briantakita/post'
 import { tag__doc_html_ } from '@btakita/ui--server--briantakita/tag'
+import { isNumber_ } from 'ctx-core/number'
 import { Elysia } from 'elysia'
 import { relement__use } from 'relementjs'
 import { server__relement } from 'relementjs/server'
@@ -100,6 +101,27 @@ export default middleware_(middleware_ctx=>
 							social_a1,
 						})
 				})))
+		.get('/posts/:slug_or_page_num', async context=>{
+			const { params: { slug_or_page_num } } = context
+			const ctx =
+				blog_server_request_ctx__ensure(
+					middleware_ctx,
+					context, {
+						logo_image,
+						site,
+						social_a1,
+					})
+			const page_num =
+				isNumber_(slug_or_page_num)
+				&& page_num_a1_(ctx)!.includes(Number(slug_or_page_num))
+					? Number(slug_or_page_num)
+					: 0
+			if (page_num) page_num__set(ctx, page_num)
+			return html_response__new(
+				page_num
+					? posts__doc_html_({ ctx })
+					: posts__doc_html_({ ctx }))
+		})
 		.get('/tags/:tag', async context=>{
 			const { params: { tag } } = context
 			const ctx =
