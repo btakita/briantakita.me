25ea05cdfeb1727a54f0f43d1800903d1bf77bdb
Brian Takita <brian.takita@gmail.com>
2024-02-07 18:29:00 -0500

    build: parallelize:
    	relysjs_browser__build
    	relysjs_server__build
    	relysjs__ready__wait

--- src/build.ts
+++ src/build.ts
@@ -16,30 +16,31 @@ export async function build(config?:relysjs__build_config_T) {
 	const rebuild_tailwind_plugin = rebuild_tailwind_plugin_({
 		tailwindcss_config
 	})
-	await relysjs_browser__build({
-		...config ?? {},
-		plugins: [rebuild_tailwind_plugin],
-	})
-	await relysjs_server__build({
-		...config ?? {},
-		target: 'es2022',
-		external: await server_external_(),
-		plugins: [
-			esmcss_esbuild_plugin_(),
-			rebuild_tailwind_plugin,
-		],
-	})
-	await relysjs__ready__wait()
+	await Promise.all([
+		relysjs_browser__build({
+			...config ?? {},
+			plugins: [rebuild_tailwind_plugin],
+		}),
+		relysjs_server__build({
+			...config ?? {},
+			target: 'es2022',
+			external: await server_external_(),
+			plugins: [
+				esmcss_esbuild_plugin_(),
+				rebuild_tailwind_plugin,
+			],
+		}),
+		relysjs__ready__wait()
+	])
 }
 function server_external_() {
-	return readdir(
-		join(
-			dirname(new URL(import.meta.url).pathname),
-			'..',
-			'..',
-			'..',
-			'node_modules'),
-	).then(file_a1=>
+	return readdir(join(
+		dirname(new URL(import.meta.url).pathname),
+		'..',
+		'..',
+		'..',
+		'node_modules'
+	)).then(file_a1=>
 		file_a1
 			.filter(file=>file !== '@btakita' && file !== '@rappstack')
 			.map(file=>file[0] === '@' ? file + '/*' : file))
