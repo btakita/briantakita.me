6caf3f16e72db18a2f9f0873bb712770054cb94d
Brian Takita <brian.takita@gmail.com>
2024-02-09 23:37:42 -0500

    app: + ∋ compression_middleware_

--- app/index.ts
+++ app/index.ts
@@ -2,10 +2,19 @@ import { import_meta_env_ } from 'ctx-core/env'
 import { Elysia } from 'elysia'
 import { relement__use } from 'relementjs'
 import { server__relement } from 'relementjs/server'
-import { app_ctx, cwd__set, is_prod_, port__set, src_path__set, static_middleware_ } from 'relysjs/server'
+import {
+	app_ctx,
+	compression_middleware_,
+	cwd__set,
+	is_prod_,
+	port__set,
+	src_path__set,
+	static_middleware_
+} from 'relysjs/server'
 export default async ()=>{
 	config__init()
 	return new Elysia()
+		.use(compression_middleware_())
 		.use(await static_middleware_(
 			is_prod_(app_ctx)
 				? {
