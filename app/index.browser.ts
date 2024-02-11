7088b378149094bc700673be1159447005411a8c
Brian Takita <brian.takita@gmail.com>
2024-02-10 21:51:20 -0500

    update dependencies

--- app/index.browser.ts
+++ app/index.browser.ts
@@ -1,7 +1,7 @@
 import { blog_header__bind } from '@rappstack/ui--browser--blog/header'
 import { theme__toggle_button__bind } from '@rappstack/ui--browser--blog/theme'
-import { hy_op } from 'relementjs/browser/hy'
-hy_op(
+import { single_hyop } from 'relementjs/browser/hy'
+single_hyop(
 	document, {
 		blog_header: blog_header__bind,
 		theme_toggle_button: theme__toggle_button__bind,
