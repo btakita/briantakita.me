f76e34ec4d7ca5d53ef76afb0f91d3314fd0d10b
Brian Takita <brian.takita@gmail.com>
2024-02-11 22:13:24 -0500

    /,/search: browser:
    	blog_header: blog_header__bind:
    		→ blog_header__hyop
    	theme_toggle_button: theme__toggle_button__bind:
    		→ theme__toggle_button__hyop
    
    /search: browser:
    
    	blog_search__main: blog_search__main__bind:
    		→ blog_search__main__hyop

--- app/index.browser.ts
+++ app/index.browser.ts
@@ -1,8 +1,8 @@
-import { blog_header__bind } from '@rappstack/ui--browser--blog/header'
-import { theme__toggle_button__bind } from '@rappstack/ui--browser--blog/theme'
+import { blog_header__hyop } from '@rappstack/ui--browser--blog/header'
+import { theme__toggle_button__hyop } from '@rappstack/ui--browser--blog/theme'
 import { single_hyop } from 'relementjs/browser/hy'
 single_hyop(
 	document, {
-		blog_header: blog_header__bind,
-		theme_toggle_button: theme__toggle_button__bind,
+		blog_header__hyop,
+		theme__toggle_button__hyop,
 	})
--- app/search/index.browser.ts
+++ app/search/index.browser.ts
@@ -1,13 +1,13 @@
-import { blog_header__bind } from '@rappstack/ui--browser--blog/header'
-import { blog_search__main__bind } from '@rappstack/ui--browser--blog/search'
-import { theme__toggle_button__bind } from '@rappstack/ui--browser--blog/theme'
+import { blog_header__hyop } from '@rappstack/ui--browser--blog/header'
+import { blog_search__main__hyop } from '@rappstack/ui--browser--blog/search'
+import { theme__toggle_button__hyop } from '@rappstack/ui--browser--blog/theme'
 import { relement__use } from 'relementjs'
 import { browser__relement, single_hyop } from 'relementjs/browser'
 relement__use(browser__relement)
 single_hyop(
 	document,
 	{
-		blog_header: blog_header__bind,
-		blog_search__main: blog_search__main__bind,
-		theme_toggle_button: theme__toggle_button__bind
+		blog_header__hyop,
+		blog_search__main__hyop,
+		theme__toggle_button__hyop,
 	})
