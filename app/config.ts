19e183bb25d12ee2eb348327b17841342f4826b8
Brian Takita <brian.takita@gmail.com>
2024-02-20 00:51:24 -0500

    ./app: config: social_a1: Github: link_title: fix: remove leading space

--- app/config.ts
+++ app/config.ts
@@ -24,7 +24,7 @@ export const social_a1:social_T[] = [
 	{
 		name: 'Github',
 		href: 'https://github.com/btakita',
-		link_title: ` ${site.title} on Github`,
+		link_title: `${site.title} on Github`,
 		active: true,
 	},
 	{
