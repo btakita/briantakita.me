85d2a7da25fcf6aa025436042c0c4bdedc91ed65
Brian Takita <brian.takita@gmail.com>
2024-03-05 23:29:31 -0500

    /posts/squarespace-site-lightouse-scores:
    	+ shoptimizer
    	wording
    	∋ nofollow_tb_a_

--- post/content/2024-03-05-squarespace-site-lightouse-scores.md.ts
+++ post/content/2024-03-05-squarespace-site-lightouse-scores.md.ts
@@ -1,6 +1,6 @@
 import { heroicons_chevron_double_right_ } from '@btakita/ui--server--briantakita/icon'
 import { post_meta__validate } from '@rappstack/domain--any--blog/post'
-import { tb_a_ } from '@rappstack/ui--any/anchor'
+import { nofollow_tb_a_, tb_a_ } from '@rappstack/ui--any/anchor'
 import { md__raw_ } from '@rappstack/ui--any/md'
 import { class_ } from 'ctx-core/html'
 import { type request_ctx_T } from 'rebuildjs/server'
@@ -10,6 +10,7 @@ import artipsstudio_html from '../../public/assets/lighthouse/artipsstudio.html'
 import devoe_html from '../../public/assets/lighthouse/devoe.html'
 import dodropshipping_html from '../../public/assets/lighthouse/dodropshipping.html'
 import jonesbbqkc_html from '../../public/assets/lighthouse/jonesbbqkc.html'
+import shoptimizer_html from '../../public/assets/lighthouse/shoptimizer.html'
 export const meta_ = ()=>post_meta__validate({
 	author: `Brian Takita`,
 	pub_date: '2024-03-06T01:41:02.185Z',
@@ -30,11 +31,11 @@ Squarespace is an E-commerce CRM with dominant market share. I became curious ab
 
 ## Site performance impacts SEO
 
-Google & other search engines ${tb_a_({ href: 'https://developers.google.com/search/blog/2010/04/using-site-speed-in-web-search-ranking', nofollow: true }, 'factor page load time')} in ranking search results. Page speed ensures the person has an optimal experience viewing the search result.
+Google & other search engines ${nofollow_tb_a_({ href: 'https://developers.google.com/search/blog/2010/04/using-site-speed-in-web-search-ranking' }, 'factor page load time')} in ranking search results. Page speed ensures the person has an optimal experience viewing the search result.
 
 ## The Lighthouse Test Tool
 
-Lighthouse is ${tb_a_({ href: 'https://developer.chrome.com/docs/lighthouse/overview/', nofollow: true }, 'built into Google Chrome')}. To use:
+Lighthouse is ${nofollow_tb_a_({ href: 'https://developer.chrome.com/docs/lighthouse/overview/' }, 'built into Google Chrome')}. To use:
 
 1. Open Chrome in Incognito Mode
 1. Navigate to the Web Page that you want to test
@@ -47,21 +48,21 @@ ${img_({
 
 Keep in mind that this site scores at 100 across the board (performance, accessibility, best practices, seo). Also keep in mind that anything above 90 is considered a good score.
 
-## Case 1 - ${tb_a_({ href: 'https://www.artipsstudio.com/', nofollow: true }, 'www.artipsstudio.com')}
+## Case 1 - ${nofollow_tb_a_({ href: 'https://www.artipsstudio.com/' }, 'www.artipsstudio.com')}
 
 A performance score of 20 is surprising. But then, this site targets fans of artistry. So large beautiful images & elegant design take priority.
 
 ${lighthouse__iframe_(artipsstudio_html)}
 
-## Case 2 - ${tb_a_({ href: 'https://www.jonesbbqkc.com/', nofollow: true }, 'www.jonesbbqkc.com')}
+## Case 2 - ${nofollow_tb_a_({ href: 'https://www.jonesbbqkc.com/' }, 'www.jonesbbqkc.com')}
 
 This site performed better but leaves much to be desired. The home page animation is catchy.
 
 ${lighthouse__iframe_(jonesbbqkc_html)}
 
-## Case 3 - ${tb_a_({ href: 'https://devoe-fluid-demo.squarespace.com/' }, 'devoe-fluid-demo.squarespace.com')}
+## Case 3 - ${nofollow_tb_a_({ href: 'https://devoe-fluid-demo.squarespace.com/' }, 'devoe-fluid-demo.squarespace.com')}
 
-${tb_a_({ href: 'https://www.stylefactoryproductions.com/blog/how-to-speed-up-a-squarespace-site', nofollow: true }, 'How to Speed Up a Squarespace Site')} features the Devoe template. For it's performance improvements over other Squarespace Site templates. With a performance score of 55, Squarespace has a ways to go.
+${nofollow_tb_a_({ href: 'https://www.stylefactoryproductions.com/blog/how-to-speed-up-a-squarespace-site' }, 'How to Speed Up a Squarespace Site')} features the Devoe template. For it's performance improvements over other Squarespace Site templates. With a performance score of 55, Squarespace has a ways to go.
 
 ${lighthouse__iframe_(devoe_html)}
 
@@ -71,15 +72,21 @@ Squarespace 7.1 has performance improvements over 7.0. So expect further perform
 
 ## Alternatives to Squarespace
 
-Alternative e-commerce platforms are another option. Though Squarespace does score well compared to it's direct competitors, such as Shopify. The ${tb_a_({ href: 'https://boostertheme.com/' }, 'Booster theme')} is the fastest Shopify theme I found for this article.
+Alternative e-commerce platforms are another option. Though Squarespace does score well compared to it's direct competitors, such as Shopify. The ${nofollow_tb_a_({ href: 'https://boostertheme.com/' }, 'Booster theme')} is the fastest Shopify theme I found for this article.
 
-## Bonus Shopify Booter Theme Case Study - ${tb_a_({ href: 'https://dodropshipping.com/', nofollow: true })}
+## Bonus Shopify Booter Theme Case Study - ${nofollow_tb_a_({ href: 'https://dodropshipping.com/' }, 'dodropshipping.com')}
 
-dodropshipping.com has a performance score of 85. A decent score! My hunch is that Shopify has an advantage due it's 3rd party theme ecosystem. These 3rd party themes boutique developers to specialize. The Booster theme team delivers high performance Shopify themes.
+dodropshipping.com has a performance score of 85. A decent score! My hunch is that Shopify has an advantage due it's 3rd party theme ecosystem. These 3rd party themes boutique developers to specialize. The Booster theme team delivers high performance Shopify themes. Dodropshipping.com is close. I would not be surprised if dodropshipping.com could optimize their page to be in the mid 90's or higher.
 
 ${lighthouse__iframe_(dodropshipping_html)}
 
-## Why are there no e-commerce themes with good (>90) to Perfect Performance Scores?
+## Bonus #2 WordPress WooCommerce Theme Shoptimizer - ${nofollow_tb_a_({ href: 'https://shoptimizerdemo.commercegurus.com/' }, 'shoptimizerdemo.commercegurus.com')}
+
+The ${nofollow_tb_a_({ href: 'https://www.commercegurus.com/product/shoptimizer/' }, 'Shoptimizer')} theme breaks into the "good" performance score with a 95! Kudos to the Shoptimizer team. Keep in mind that this is a demo & is likely the ceiling of this theme's optimization. Yet few theme achieve a performance score >90, so it is noteworthy.
+
+${lighthouse__iframe_(shoptimizer_html)}
+
+## Why are there few e-commerce themes with good (>90) to Perfect Performance Scores?
 
 An e-commerce CMS theme has to account for a wide range of use cases. Some use cases include:
 
@@ -88,11 +95,13 @@ An e-commerce CMS theme has to account for a wide range of use cases. Some use c
 - Supporting the business & staff overhead while making a profit. This often means maximizing revenue by serving many customers. And lowering costs by sharing infrastructure.
 - The site configuration must be high performance. This means not adding unnecessary javascript, css, & assets. And using non-blocking methods for page interaction. 
 
-Good to perfect performance scores with a large e-commerce CMS platform is possible. Obtaining these measurables is difficult. It will take time for e-commerce platforms to develop their codebases, practices, & infrastructures. Dodropshipping.com is close. I would not be surprised if dodropshipping.com could optimize their page to be in the mid 90's or higher.
+Good to perfect performance scores with a large e-commerce CMS platform is possible. Obtaining these measurables is difficult. It will take time for e-commerce platforms to develop their codebases, practices, & infrastructures.
 
 ## A good (>90) to Perfect Performance Score with Custom Sites or Custom Platforms
 
-A custom e-commerce site or platform can have a good to perfect performance score. With a software stack built for speed. I am working on high performance e-commerce solutions. I custom built software for performance, efficiency, & maintainability. Using cutting edge industry & novel tech. Stay tuned for an e-commerce platform that will give you a high performance. With SEO & Semantic AI optimization. Along with usability.
+A custom e-commerce site or platform can have a good to perfect performance score. With a software stack built for speed. I am working on high performance e-commerce solutions. I custom built software for performance, efficiency, & maintainability. Using cutting edge industry & novel tech. Stay tuned for an e-commerce platform that will give you a high performance. With SEO & Semantic AI optimization. Along with usability. There will be other features geared toward serving Small Business Owners.
+
+I am striving to announce this platform soon. Until then, happy optimizing!
 `)
 function lighthouse__iframe_(src:string) {
 	return iframe_({ src, class: 'w-full h-80 mb-3' })
