a9ca749e4712b5deb06ee9a232d6aa12030a51da
Brian Takita <brian.takita@gmail.com>
2024-03-27 00:00:32 -0400

    ./app: ∋ @btakita/ui--browser--briantakita/layout/hyops:
    	.
    	./posts
    	./search
    
    ∋ @btakita/ui--browser--briantakita

--- app/posts/index.browser.ts
+++ app/posts/index.browser.ts
@@ -1,9 +1,11 @@
+import * as layout__hyops from '@btakita/ui--browser--briantakita/layout/hyop'
 import * as blog_header__hyops from '@rappstack/ui--browser--blog/header/hyop'
 import * as blog_post__hyops from '@rappstack/ui--browser--blog/post/hyop'
 import * as blog_theme__hyops from '@rappstack/ui--browser--blog/theme/hyop'
 import { hyop } from 'relementjs/browser'
 window.addEventListener('load', ()=>{
 	hyop(document, {
+		...layout__hyops,
 		...blog_header__hyops,
 		...blog_post__hyops,
 		...blog_theme__hyops,
--- app/search/index.browser.ts
+++ app/search/index.browser.ts
@@ -1,3 +1,4 @@
+import * as layout__hyops from '@btakita/ui--browser--briantakita/layout/hyop'
 import * as blog_header__hyops from '@rappstack/ui--browser--blog/header/hyop'
 import * as blog_search__hyops from '@rappstack/ui--browser--blog/search/hyop'
 import * as blog_theme__hyops from '@rappstack/ui--browser--blog/theme/hyop'
@@ -6,6 +7,7 @@ import { browser__relement, hyop } from 'relementjs/browser'
 window.addEventListener('load', ()=>{
 	relement__use(browser__relement)
 	hyop(document, {
+		...layout__hyops,
 		...blog_header__hyops,
 		...blog_search__hyops,
 		...blog_theme__hyops,
