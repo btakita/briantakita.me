4b503c623f70f7ed62b62e889226f245b450acec
Brian Takita <brian.takita@gmail.com>
2024-05-31 19:09:54 -0400

    ./ctx: briantakita_request_ctx__ensure: + ∋ breadcrumbs__nav_class__set

--- ctx/briantakita_request_ctx.ts
+++ ctx/briantakita_request_ctx.ts
@@ -5,8 +5,10 @@ import {
 	Person_id_ref_,
 	WebSite_id_ref_
 } from '@btakita/domain--server--briantakita/jsonld'
+import { sticky__top__nav_class } from '@btakita/ui--server--briantakita/sticky'
 import { blog_request_ctx__ensure, type blog_request_ctx__ensure_config_T } from '@rappstack/domain--server--blog/ctx'
 import { marked__set } from '@rappstack/ui--any/md'
+import { breadcrumbs__nav_class__set } from '@rappstack/ui--server--blog/breadcrumb'
 import { app_marked_ } from '@rappstack/ui--server/md'
 import type { middleware_ctx_T } from 'rebuildjs/server'
 export function briantakita_request_ctx__ensure(
@@ -17,12 +19,13 @@ export function briantakita_request_ctx__ensure(
 	},
 	config:blog_request_ctx__ensure_config_T
 ) {
-	const request_ctx = blog_request_ctx__ensure(middleware_ctx, context, config)
-	Person_id_ref_(request_ctx)
-	WebSite_id_ref_(request_ctx)
-	Organization_id_ref_(request_ctx)
-	AboutPage_id_ref_(request_ctx)
-	ContactPage_id_ref_(request_ctx)
-	marked__set(request_ctx, app_marked_(request_ctx))
-	return request_ctx
+	const ctx = blog_request_ctx__ensure(middleware_ctx, context, config)
+	Person_id_ref_(ctx)
+	WebSite_id_ref_(ctx)
+	Organization_id_ref_(ctx)
+	AboutPage_id_ref_(ctx)
+	ContactPage_id_ref_(ctx)
+	marked__set(ctx, app_marked_(ctx))
+	breadcrumbs__nav_class__set(ctx, sticky__top__nav_class)
+	return ctx
 }
